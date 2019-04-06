07e72ec066ff6c86870d70caf1e377864c8372d4
manwanimonishashok <manwanimonishashok@gmail.com>
2019-04-06 12:31:03 +0530

    add method using post and api but showing error

--- class/restcalci.js
+++ class/restcalci.js
@@ -2,21 +2,25 @@
 
 var express = require ("express");
 
+var app = express();
+
 var bodyParser = require("body-parser");
 
-var app = express();
+
 app.use(bodyParser.urlencoded({ extended: false  }));
 app.use(bodyParser.json());
-app.post("/addition/:number1/:number2",function(request,response){
+
+
+app.post("/add/:num1/:num2",function(request,response){
     console.log("/add route executed ...")
     
     var data = {
         num1:parseInt(request.body.num1),
         num2:parseInt(request.body.num2),
-        result:parseInt(request.body.num1) + parseInt(request.body.num2)
+        sum:parseInt(request.body.num1) + parseInt(request.body.num2)
     };
     console.log(data);
     response.send(data);
-
 });
+
 app.listen(3000);
