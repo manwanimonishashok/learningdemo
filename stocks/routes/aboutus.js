cf2b4ef97b7e9ff0a65eaa126d89b5d6ae81fb84
manwanimonishashok <manwanimonishashok@gmail.com>
2019-05-18 10:30:26 +0530

    updated

--- stocks/routes/aboutus.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var aboutUs = function(request, response) {
-    response.render("aboutus.hbs");
-};
-
-exports.aboutUs = aboutUs;
--- stocks/routes/whystocks.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var whyStocks = function(request, response) {
-    response.render("whystocks.hbs")
-}
-
-exports.whyStocks = whyStocks;
